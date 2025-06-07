1e66122c2e62e4850d6319bf6086b261e5db0e2b
KAUE GOMES CIDRAL <kaue.cidral@univille.br>
2025-06-06 22:03:28 -0300

    Começo dos testes

---
+++ Etapa_1/Aula_1/Exercicio_1.java
@@ -0,0 +1,7 @@
+public class Exercicio_1 {
+    public static void main(String[] args) {
+        System.out.println("Meu nome é João.");
+        System.out.println("Tenho 22 anos.");
+        System.out.println("Quero dominar a linguagem Java!");
+    }
+}
---
+++ Etapa_1/Aula_1/Exercicio_2.java
@@ -0,0 +1,12 @@
+public class Exercicio_2 {
+    public static void main(String[] args) {
+    
+    int n1 = 7;
+    int n2 = 3;
+    int resultado = 0;
+
+    resultado = n1 + n2;
+
+    System.out.println("o resultado da soma de " + n1 + " e " + n2 + " = " + resultado );
+ }
+}
